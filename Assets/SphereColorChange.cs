0c9de7d1b799cd9173e7bdf634ba8d603d9d292e
paulaborregoon <paula.borregon@estudiantat.upc.edu>
2025-09-23 11:35:19 +0200

    Spheres

--- Assets/SphereColorChange.cs
+++ Assets/SphereColorChange.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class SphereColorChange : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Renderer[] spheres;     // pots omplir-ho manualment a l'Inspector
+    [SerializeField] private float delay = 0.5f;     // retard entre rotacions (segons)
+    [SerializeField] private bool autoCollectChildren = true; // si true, recull els Renderers dels fills
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         
