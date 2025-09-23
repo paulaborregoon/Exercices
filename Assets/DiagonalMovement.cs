c1a8b3abcc00d48fa72bcf37c009121642022a7e
paulaborregon <paula.borregon01@estudiant.upf.edu>
2025-09-23 23:15:27 +0200

    Diagonal Movement

---
+++ Assets/DiagonalMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DiagonalMovement : MonoBehaviour
+{
+    public float velocitat = 2f; //velocitat a la que es mou
+
+    void Update()
+    {
+        Vector3 moviment = new Vector3(1, 1, 0) * velocitat * Time.deltaTime;
+        transform.Translate(moviment, Space.World);
+
+        // Vector3 es X,Y,Z i va a update pq es va actualitzant cada cop
+        // Time.deltaTime = 0.016f (60 FPS → 1/60 = 0.016 segons per frame)
+    }
+}
